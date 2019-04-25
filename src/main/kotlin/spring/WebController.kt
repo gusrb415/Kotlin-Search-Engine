e84178ceb7fea8509b475347a52adcf5e6d447c6
gusrb <gusrb415@gmail.com>
2019-04-25 22:46:51 +0800

    Added result returning to UI

--- src/main/kotlin/spring/WebController.kt
+++ src/main/kotlin/spring/WebController.kt
@@ -2,6 +2,7 @@ package spring
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +16,12 @@ class WebController {
     }
 
     @PostMapping("/web")
-    fun webSubmit(@ModelAttribute web: Web): String {
+    fun webSubmit(@ModelAttribute web: Web, result: BindingResult, map: ModelMap): String {
+        if(result.hasErrors()) return "index"
+        val query = web.content
+        //This is where you get query
+        //Parse it, modify it do whatever then return
+        map.addAttribute("result", "this is the result string with $query")
         return "result" // return src/main/resources/templates/result.html
     }
 }
